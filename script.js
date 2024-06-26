628b61aa1ecf4789e0d3fd0d8e9f4cb2f70ef1f1
SriMoorthy <srimoorthy27@gmail.com>
2024-06-26 10:52:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	let S1 = s1.toLowerCase()
+	let S2 = s2.toLowerCase()
+	let flag = false
+	for (let i = 0; i < S2.length; i++) {
+		for (let j = 0; j < S1.length; j++) {
+			if (S2[i]==S1[j] && S2[i+1]==S1[j+1]) {
+				return j
+				flag = true
+				break
+			}
+		}
+	}
+	if(flag == false){
+		return -1
+	}
 }
 
 // Please do not change the code below
